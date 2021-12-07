46e502536fa44fb86a067bc99ef166b33d836d58
ozzie-hunter <72672562+ozzie-hunter@users.noreply.github.com>
2021-12-07 21:48:38 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
+let Mapped_Drive2 = 0
 let Mapped_Drive = 0
 let Turn = 0
 let Drive = 0
 let Enable_Drive = false
 if (!(Enable_Drive)) {
-    basic.showIcon(IconNames.Happy)
     Enable_Drive = true
     basic.showIcon(IconNames.SmallSquare)
 }
@@ -11,6 +11,15 @@ basic.forever(function () {
     Drive = input.rotation(Rotation.Pitch)
     Turn = input.rotation(Rotation.Roll)
     if (Enable_Drive) {
+        if (Drive < 20 && Drive > -20 && (Turn < 20 && Turn > -20)) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        }
         if (Drive < -20) {
             Mapped_Drive = pins.map(
             Drive,
@@ -41,21 +50,13 @@ basic.forever(function () {
                 . # # # .
                 . . # . .
                 `)
-        } else {
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                . # # # .
-                . # # # .
-                . . . . .
-                `)
         }
         if (Turn < -20) {
-            Mapped_Drive = pins.map(
+            Mapped_Drive2 = pins.map(
             Turn,
+            0,
             -90,
-            90,
-            -100,
+            0,
             100
             )
             basic.showLeds(`
@@ -63,6 +64,21 @@ basic.forever(function () {
                 . # # . .
                 # # # # #
                 . # # . .
+                . . # . .
+                `)
+        } else if (Turn > 20) {
+            Mapped_Drive2 = pins.map(
+            Turn,
+            0,
+            90,
+            0,
+            100
+            )
+            basic.showLeds(`
+                . . # . .
+                . . # # .
+                # # # # #
+                . . # # .
                 . . # . .
                 `)
         }
